dd5ca95b3f554d3c9a051b14e577f337a77558e6
Tom Freudenmann <tom.freudenmann@outlook.de>
2022-12-21 15:20:33 +0100

    - ADD: First two chapters and research of data

--- test-titanic.R
+++
@@ -1,8 +0,0 @@
-passangers <- read.csv("data/titanic/train.csv")
-dim(passangers)
-
-View(passangers)
-ggplot(data = passangers) +
-  geom_boxplot(aes(x = Survived, group = Pclass ))
-
-qplot(Pclass, Survived, data = passangers, geom = "boxplot")
